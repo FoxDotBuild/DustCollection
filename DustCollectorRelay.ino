1aa56faf05a4ded2d56dc717af3cf9165af20a44
kimber2 <34964678+kimber2@users.noreply.github.com>
2020-10-01 13:56:32 -0500

    Add files via upload
    There are 2 files here:
    
     DustCollectionRelay runs on an ESP32 with an OLED. The board type is LOLIN ESP32. This board sets up a Soft Access Point and looks for Telnet messages to turn the relay either on or off.
    
    DustCollectionRemote runs on an ESP8266. This board connects to the ESP32 via WiFi. Based on button pushed it sends Telnet commands to either turn on or off the relay.

---
+++ DustCollectorRelay.ino
@@ -0,0 +1,257 @@
+/* Dust Collector Relay
+ * 
+ * Used to turn on/off the dust collector via WiFi from remotes at each machine
+ * This program runs at the dust collector and controls the power relay.
+ * A separate program runs in each remote. Remotes are located by each machine
+ * so the collection system can be turned on conveniently.
+ * 
+ * Written by Doug Kimber
+ */
+
+// This program is to be uploaded onto the ESP32 arduino. 
+
+
+#include <WiFi.h>
+#include "SSD1306.h"    // Header file for OLED
+
+// Declare the OLED Display object
+SSD1306 display( 0x3c, 5, 4 );
+
+// Arrays to store the strings on each line of the display
+// Max is 4 lines of 16 chars (really more like 14 chars)
+#define DISP_WIDTH 16
+#define DISP_LINES 4
+#define LINE_HEIGHT 16    // A line is 16 pixels tall
+
+typedef struct Line
+{
+  int offset; // Vertical offset in pixels of this line
+  char string[ DISP_WIDTH ];
+} LINE;
+
+LINE Lines[ DISP_LINES ];
+
+int lineCount;  // Current # of displayed lines
+
+#define AP_SSID "DustCollector"
+#define AP_PWD "Breathe"
+#define PORT 23
+
+/* Set these to your desired AP credentials. */
+const char *APssid = AP_SSID;
+const char *APpassword = AP_PWD;
+int port = PORT;
+
+// pin assignments
+const int RelayPin = 16;
+
+// Buffer for an incoming line from the serial port
+// Serial I/O is event (interrupt) driven
+char lineBuf[ 256 ];
+int lineIdx;
+
+// declare telnet server (do NOT put in setup())
+WiFiServer TelnetServer( port );
+WiFiClient Telnet;
+
+// Implement a scrolling display - scrolls lines up the screen
+void DisplayLine( char* theLine )
+{
+  int i;
+
+  // If this is the first line then clear the display
+  if( lineCount == 0 )
+    display.clear();
+    
+  // Decide if we need to scroll lines up
+  if( lineCount >= DISP_LINES )
+  {
+    // Need to scroll up. Delete top line and move all others up
+    for( i = 0; i < DISP_LINES - 1; i++ )
+    {
+      strcpy( Lines[ i ].string, Lines[ i + 1 ].string );
+    }
+    strcpy( Lines[ DISP_LINES - 1 ].string, theLine );
+
+    // Now display the lines
+    display.clear();
+    for( i = 0; i < DISP_LINES; i++ )
+    {
+      display.drawString( 0, Lines[ i ].offset, Lines[ i ].string );
+    }
+  }
+  else
+  {
+    // Save the characters of the new line
+    strcpy( Lines[ lineCount ].string, theLine );
+    display.drawString( 0, Lines[ lineCount ].offset, theLine );
+    lineCount++;
+  }
+  display.display();
+}
+
+void doChar( char c )
+{
+  // Is this the end of a line?
+  if( (c == '\n') || (c == '\r') )
+  {
+    // This is the end of an input line
+    processLine();
+    lineIdx = 0;
+  }
+  else
+  {
+    lineBuf[ lineIdx ] = c;
+    lineIdx++;
+    lineBuf[ lineIdx ] = '\0';  // Null terminate for safety
+  }
+ 
+}
+
+void handleTelnet(){
+  if( TelnetServer.hasClient() ){
+    // client is connected
+    if( !Telnet || !Telnet.connected() ){
+      if( Telnet ) Telnet.stop();          // client disconnected
+      Telnet = TelnetServer.available(); // ready for new client
+    } else {
+      TelnetServer.available().stop();  // have client, block new conections
+      DisplayLine( "New Connect" );
+    }
+  }
+
+  if( Telnet && Telnet.connected() && Telnet.available()){
+    // client input processing
+    while( Telnet.available() )
+    {
+      char c = Telnet.read();
+      c = c & 0x7f;         // Strip parity bit
+      doChar( c );  // Add character to the buffer, handle command if command is complete
+    }
+  } 
+}
+
+void startAP()
+{
+  Serial.println();
+  Serial.println("Starting AP");
+
+  WiFi.mode( WIFI_AP );
+  IPAddress staticIP( 192, 168, 10, 2 );
+  IPAddress gateway( 192, 168, 10, 1 );
+  IPAddress subnet( 255, 255, 255, 0 );
+  WiFi.softAPConfig( staticIP, gateway, subnet );
+  //WiFi.softAPConfig( staticIP );
+  WiFi.softAP( APssid ); // Open network without passing password parameter
+
+  //WiFi.softAPConfig( staticIP, gateway, subnet );
+  
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+  
+  //Serial.print( "IP address: " );
+  //Serial.println( staticIP );
+  //Serial.println( WiFi.localIP() );
+  
+  display.drawString( 0, Lines[ 1 ].offset, staticIP.toString() );
+  display.display();
+  
+  delay(4000); // ap delay
+}
+
+void StartDisplay()
+{
+  display.init();
+  display.flipScreenVertically();
+  display.setFont( ArialMT_Plain_16 );  
+}
+void setup()
+{
+  int i;
+
+  Serial.begin( 115200 );
+  
+  pinMode( RelayPin, OUTPUT ); 
+
+  delay(1000); // serial delay
+
+  //Set up the array that holds line characters so we can scroll
+  for( i = 0; i < DISP_LINES; i++ )
+  {
+    Lines[ i ].offset = i * LINE_HEIGHT;
+  }
+  lineCount = 0;
+
+  StartDisplay();
+  display.drawString( 0, 0, "Dust Collector" );
+  display.display();
+
+  startAP();   // startup is async
+
+  TelnetServer.begin( );
+  
+  Serial.print("Starting telnet server on port " + (String)port);
+  //TelnetServer.setNoDelay(true); // ESP BUG ?
+  Serial.println();
+  delay(100);
+
+  // Initialize global variables
+  lineIdx = 0;
+}
+
+// Process a single command line
+void processLine()
+{
+  int val;
+
+  DisplayLine( lineBuf );
+
+  // Accept only 2 commands, ignore everything else
+  // One side of relay is connected to power, the other to RelayPin
+  // Thus, driving RelayPin low turns on the relay
+  if( strcmp( lineBuf, "DustFree" ) == 0 )
+  {
+    // Turn on the relay
+    Serial.println("Relay On!");
+    digitalWrite (RelayPin, LOW);
+  }
+
+  if( strcmp( lineBuf, "Dusty" ) == 0 )
+  {
+    // Turn on the relay
+    Serial.println("Relay Off!");
+    digitalWrite (RelayPin, HIGH);
+  }
+
+  lineIdx = 0;
+}
+
+int loopCount = 0;
+
+void loop()
+{
+  // See if we got any wireless data
+  handleTelnet();
+
+  delay(200); // Give recipient time to process any transmissions
+
+  // Restart WiFi and the display periodically
+  loopCount++;
+  if( loopCount >= 500 ) // > 10 seconds with 200mS delay above
+  {
+    Serial.println( "Restarting Display" );
+    StartDisplay();
+    Serial.println( "Restarting WiFi" );
+    //ESP.restart();    // Can't reboot - need to rpeserve the relay state
+    Telnet.stop();
+    WiFi.disconnect();
+    startAP();   // startup is async
+    delay( 2000 );
+    TelnetServer.begin( );
+    delay( 2000 );
+    loopCount = 0; 
+  }
+
+  delay( 1000 );
+}
---
+++ DustCollectorRemote.ino
@@ -0,0 +1,123 @@
+/* Dust Collector Remote
+ * 
+ * Used to turn on/off the dust collector via WiFi from remotes at each machine
+ * This program is the remote control.
+ * A separate program runs at the dust collector. Remotes are located by each machine
+ * so the collection system can be turned on conveniently.
+ * 
+ * Written by Doug Kimber
+ */
+
+// This program is to be uploaded onto the ESP8266 arduino. 
+
+
+#include <ESP8266WiFi.h>
+
+#define AP_SSID "DustCollector"
+#define AP_PWD "Breathe"
+
+#define PORT 23
+
+// Set these to match the Dust Collector Relay
+char *APssid = AP_SSID;
+char *APpassword = AP_PWD;
+int port = PORT;
+
+// ??? pin assignments
+const int OffPin = 4;
+const int OnPin  = 5;
+
+int Status = WL_IDLE_STATUS;
+IPAddress Server( 192, 168, 10, 2 );    // Dust Collector Relay
+
+// declare telnet client (do NOT put in setup())
+WiFiClient Telnet;
+
+void ConnectTelnet()
+{
+  if( Telnet.connect( Server, 23 ) )
+  {
+    Serial.println( "Telnet Connected" );
+  }
+  else
+  {
+    Serial.println( "Telnet Connection Failed!" );  
+  }
+}
+
+void ConnectWiFi()
+{
+  //bool Auto = WiFi.getAutoConnect();
+  //Serial.print( "Autoconnect: " );
+  //Serial.println( Auto );
+  
+  WiFi.setAutoConnect( false );     // Reconnect via code  
+  WiFi.hostname( "APssid" );        // This is necessary, but not clear why  
+  WiFi.begin( APssid );             // Connect to the network
+
+  Serial.print("Connecting to ");
+  Serial.print( APssid ); Serial.println(" ...");
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++i); Serial.print(' ');
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");  
+  Serial.print("IP address:\t");
+  Serial.println( WiFi.localIP() );         // Send the IP address of the ESP8266 to the computer
+
+  delay( 4000 );
+  ConnectTelnet(); 
+}
+void setup()
+{
+  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  delay(500);
+  Serial.println('\n');
+
+  // Make the On and Off pins inputs
+  pinMode( OnPin,  INPUT_PULLUP ); 
+  pinMode( OffPin, INPUT_PULLUP ); 
+
+  ConnectWiFi();
+}
+
+void loop()
+{
+  // Check for either the ON or OFF buttons being pressed
+
+  // In Case of a WiFi disconnection, reconnect
+  if( WiFi.status() != WL_CONNECTED )
+  {
+    ConnectWiFi();
+  }
+  else
+  {
+    if( Telnet.connected() != true )
+    {
+      ConnectTelnet();
+    }
+  }
+ 
+  if( digitalRead( OffPin ) == LOW )
+  {
+    Serial.println( "Turning Off!" );
+    Telnet.print( "Dusty\n" );
+    delay( 1000 );
+  }
+
+  if( digitalRead( OnPin ) == LOW )
+  {
+    Serial.println( "Turning On!" );
+    Telnet.print( "DustFree\n" );
+    delay( 1000 );
+  }
+
+  delay( 1000 );
+
+  
+
+}
