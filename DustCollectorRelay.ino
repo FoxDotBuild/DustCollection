f5f6a15b9de099074969450ed84dcad3ca174b82
kimber2 <34964678+kimber2@users.noreply.github.com>
2025-01-30 17:20:17 -0600

    Add files via upload
    Added heartbeat mechanisms, lowered threshold for tablesaw, allowed 270* servos in addition to 180* servos.

--- DustCollectorRelay.ino
+++ DustCollectorRelay.ino
@@ -1,6 +1,6 @@
 /* Dust Collector Relay
  * 
- * Used to turn on/off the dust collector via WiFi from remotes at each machine
+ * Used to turn on/off the dust collector via WiFi from remotes at each machine.
  * This program runs at the dust collector and controls the power relay.
  * A separate program runs in each remote. Remotes are located by each machine
  * so the collection system can be turned on conveniently.
@@ -11,28 +11,40 @@
 // This program is to be uploaded onto the ESP32 arduino.
 // Board type is WEMOS LOLIN32 
 
+#define SERIAL_DEBUG
 
 #include <WiFi.h>
+//#include <WiFiMulti.h>
 #include "SSD1306.h"    // Header file for OLED
 
 // Declare the OLED Display object
+// The display is 128x64
+// The code divides it into 4 lines of 16 bits in height each
+// So lines start at Y values of 0, 16, 32, and 48
 SSD1306 display( 0x3c, 5, 4 );
 
-// Arrays to store the strings on each line of the display
-// Max is 4 lines of 16 chars (really more like 14 chars)
-#define DISP_WIDTH 16
-#define DISP_LINES 4
-#define LINE_HEIGHT 16    // A line is 16 pixels tall
-
-typedef struct Line
+// Bitmaps for heartbeart hearts
+// Generated from http://dotmatrixtool.com/
+const u_char tinyHeart[] PROGMEM =
 {
-  int offset; // Vertical offset in pixels of this line
-  char string[ DISP_WIDTH ];
-} LINE;
-
-LINE Lines[ DISP_LINES ];
-
-int lineCount;  // Current # of displayed lines
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xe0, 0x00, 0xf0, 0x01, 0xc0, 0x03, 0xc0, 0x03, 0xf0, 0x01, 0xe0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+const u_char smallHeart[] PROGMEM =
+{
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xe0, 0x00, 0xf0, 0x01, 0xf8, 0x03, 0xe0, 0x07, 0xe0, 0x07, 0xf8, 0x03, 0xf0, 0x01, 0xe0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+const u_char mediumHeart[] PROGMEM =
+{
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xe0, 0x00, 0xf0, 0x01, 0xf8, 0x03, 0xf8, 0x07, 0xe0, 0x0f, 0xe0, 0x0f, 0xf8, 0x07, 0xf8, 0x03, 0xf0, 0x01, 0xe0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+const u_char largeHeart[] PROGMEM =
+{
+0x00, 0x00, 0x00, 0x00, 0xe0, 0x00, 0xf0, 0x01, 0xf8, 0x03, 0xfc, 0x07, 0xfc, 0x0f, 0xf0, 0x1f, 0xf0, 0x1f, 0xfc, 0x0f, 0xfc, 0x07, 0xf8, 0x03, 0xf0, 0x01, 0xe0, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+const u_char giantHeart[] PROGMEM =
+{
+0x00, 0x00, 0xe0, 0x00, 0xf0, 0x01, 0xf8, 0x03, 0xfc, 0x07, 0xfc, 0x0f, 0xfc, 0x1f, 0xf0, 0x3f, 0xf0, 0x3f, 0xfc, 0x1f, 0xfc, 0x0f, 0xfc, 0x07, 0xf8, 0x03, 0xf0, 0x01, 0xe0, 0x00, 0x00, 0x00
+};
 
 #define AP_SSID "DustCollector"
 #define AP_PWD "Breathe"
@@ -46,128 +58,87 @@ int port = PORT;
 // pin assignments
 const int RelayPin = 16;
 
-// Buffer for an incoming line from the serial port
-// Serial I/O is event (interrupt) driven
-char lineBuf[ 256 ];
-int lineIdx;
-
 // declare telnet server (do NOT put in setup())
+#define MAX_CLIENTS 4
 WiFiServer TelnetServer( port );
-WiFiClient Telnet;
+WiFiClient Clients[ MAX_CLIENTS ];
 
-// Implement a scrolling display - scrolls lines up the screen
-void DisplayLine( char* theLine )
+// Keep track of the time since the last message from each client
+#define HB_INTERVAL 3000    // Maximum Milliseconds between heartbeats
+unsigned long ClientHB[ MAX_CLIENTS ];
+
+// Heartbeat and On/Off status for each client
+bool HBstat[ MAX_CLIENTS ];
+bool On[ MAX_CLIENTS ];
+// Locations on the display for heartbeats and channel status
+typedef struct
 {
-  int i;
-
-  // If this is the first line then clear the display
-  if( lineCount == 0 )
-    display.clear();
-    
-  // Decide if we need to scroll lines up
-  if( lineCount >= DISP_LINES )
-  {
-    // Need to scroll up. Delete top line and move all others up
-    for( i = 0; i < DISP_LINES - 1; i++ )
-    {
-      strcpy( Lines[ i ].string, Lines[ i + 1 ].string );
-    }
-    strcpy( Lines[ DISP_LINES - 1 ].string, theLine );
-
-    // Now display the lines
-    display.clear();
-    for( i = 0; i < DISP_LINES; i++ )
-    {
-      display.drawString( 0, Lines[ i ].offset, Lines[ i ].string );
-    }
-  }
-  else
-  {
-    // Save the characters of the new line
-    strcpy( Lines[ lineCount ].string, theLine );
-    display.drawString( 0, Lines[ lineCount ].offset, theLine );
-    lineCount++;
-  }
-  display.display();
-}
-
-void doChar( char c )
+  int HBx;    // X location of heartbeat icon
+  int HBy;    // Y location of heartbeat icon
+  int StatX;  // X location of ON/Off Status
+  int StatY;  // Y location of ON/Off Status
+} DISP_LOCS;
+DISP_LOCS Disp_locs[ MAX_CLIENTS ] =
 {
-  // Is this the end of a line?
-  if( (c == '\n') || (c == '\r') )
-  {
-    // This is the end of an input line
-    processLine();
-    lineIdx = 0;
-  }
-  else
-  {
-    lineBuf[ lineIdx ] = c;
-    lineIdx++;
-    lineBuf[ lineIdx ] = '\0';  // Null terminate for safety
-  }
- 
-}
+  {  0, 32, 16, 32 },
+  { 64, 32, 80, 32 },
+  {  0, 48, 16, 48 },
+  { 64, 48, 80, 48 },
 
-void handleTelnet(){
-  if( TelnetServer.hasClient() ){
-    // client is connected
-    if( !Telnet || !Telnet.connected() ){
-      if( Telnet ) Telnet.stop();          // client disconnected
-      Telnet = TelnetServer.available(); // ready for new client
-    } else {
-      TelnetServer.available().stop();  // have client, block new conections
-      DisplayLine( "New Connect" );
-      Telnet = TelnetServer.available(); // ready for new client
-    }
-  }
+};
 
-  if( Telnet && Telnet.connected() && Telnet.available()){
-    // client input processing
-    while( Telnet.available() )
-    {
-      char c = Telnet.read();
-      c = c & 0x7f;         // Strip parity bit
-      doChar( c );  // Add character to the buffer, handle command if command is complete
-    }
-  } 
-}
+// Buffers for messages from each client
+#define MAX_LINE 32
+typedef struct
+{
+  int index;
+  //int unload;
+  char buf[ MAX_LINE ];
+}BUFFER;
 
+BUFFER bufs[ MAX_CLIENTS ];
+
+//-------------------------------------------------------------------
 void startAP()
 {
   Serial.println();
   Serial.println("Starting AP");
 
   WiFi.mode( WIFI_AP );
+  delay( 100 );
+  WiFi.setSleep( false );
+  delay( 100 );
   IPAddress staticIP( 192, 168, 10, 2 );
   IPAddress gateway( 192, 168, 10, 1 );
   IPAddress subnet( 255, 255, 255, 0 );
+  delay( 100 );
   WiFi.softAPConfig( staticIP, gateway, subnet );
+  delay( 100 );
   //WiFi.softAPConfig( staticIP );
   WiFi.softAP( APssid ); // Open network without passing password parameter
-
+  delay( 100 );
+  WiFi.begin();
+  delay( 1000 );
+  
   //WiFi.softAPConfig( staticIP, gateway, subnet );
   
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
-  Serial.println(IP);
+  Serial.println( IP );
   
-  //Serial.print( "IP address: " );
-  //Serial.println( staticIP );
-  //Serial.println( WiFi.localIP() );
-  
-  display.drawString( 0, Lines[ 1 ].offset, staticIP.toString() );
+  display.drawString( 0, 16, staticIP.toString() );
   display.display();
   
   delay(1000); // ap delay
 }
-
+//-------------------------------------------------------------------
 void StartDisplay()
 {
   display.init();
   display.flipScreenVertically();
   display.setFont( ArialMT_Plain_16 );  
 }
+//-------------------------------------------------------------------
 void setup()
 {
   int i;
@@ -175,15 +146,9 @@ void setup()
   Serial.begin( 115200 );
   
   pinMode( RelayPin, OUTPUT ); 
+  digitalWrite (RelayPin, HIGH );
 
-  delay(1000); // serial delay
-
-  //Set up the array that holds line characters so we can scroll
-  for( i = 0; i < DISP_LINES; i++ )
-  {
-    Lines[ i ].offset = i * LINE_HEIGHT;
-  }
-  lineCount = 0;
+  delay( 1000 ); // serial delay
 
   StartDisplay();
   display.drawString( 0, 0, "Dust Collector" );
@@ -192,70 +157,267 @@ void setup()
   startAP();   // startup is async
 
   TelnetServer.begin( );
+  //TelnetServer.setNoDelay( true );
   
   Serial.print("Starting telnet server on port " + (String)port);
-  //TelnetServer.setNoDelay(true); // ESP BUG ?
+  TelnetServer.setNoDelay( true ); // ESP BUG ?
   Serial.println();
+
+  // Initialize the heartbeat timers, etc.
+  for( int i = 0; i < MAX_CLIENTS; i++ )
+  {
+    //Clients[ i ] = nullptr;
+    ClientHB[ i ] = millis();
+    bufs[ i ].index = 0;
+    HBstat[ i ] = false;
+    On[ i ] = false;
+    drawHB( i );
+    drawStatus( i );
+  }
+
   delay(100);
-
-  // Initialize global variables
-  lineIdx = 0;
 }
-
-// Process a single command line
-void processLine()
+//-------------------------------------------------------------------
+void drawHB( int index )
 {
-  int val;
+  int x, y;
 
-  DisplayLine( lineBuf );
+  x = Disp_locs[ index ].HBx;
+  y = Disp_locs[ index ].HBy;
+  if( HBstat[ index ] == false )
+  {
+    display.setColor( BLACK );
+    display.fillRect( x, y, 16, 16 );
+    display.setColor( WHITE );
+    display.drawFastImage( x, y, 16, 16, (const char*)smallHeart );
+    display.display();
+  }
+  else
+  {
+    display.setColor( BLACK );
+    display.fillRect( x, y, 16, 16 );
+    display.setColor( WHITE );
+    display.drawFastImage( x, y, 16, 16, (const char*)giantHeart );
+    display.display();
+  }
+}
+//-------------------------------------------------------------------
+void drawStatus( int index )
+{
+  int x, y;
 
-  // Accept only 2 commands, ignore everything else
+  x = Disp_locs[ index ].StatX;
+  y = Disp_locs[ index ].StatY;
+  if( On[ index ] == false )
+  {
+    display.setColor( BLACK );
+    display.fillRect( x, y, 32, 16 );
+    display.setColor( WHITE );
+    display.drawString( x, y, "Off" );
+    display.display();
+  }
+  else
+  {
+    display.setColor( BLACK );
+    display.fillRect( x, y, 32, 16 );
+    display.setColor( WHITE );
+    display.drawString( x, y, "On" );
+    display.display();
+  }
+}
+//-------------------------------------------------------------------
+void TurnOff()
+{    
+  // Turn off the relay only if all Clients agree
+  bool TurnOff = true;
+  for( int i = 0; i < MAX_CLIENTS; i++ )
+  {
+    if( On[ i ] == true )
+      TurnOff = false;
+  }
+  if( TurnOff == true )
+  {
+    Serial.println( "All Off!" );
+    digitalWrite ( RelayPin, HIGH ) ;
+  }
+}
+//-------------------------------------------------------------------
+// Process a single command line
+void processLine( char* Buf, int index )
+{
+  // Accept only 3 commands, ignore everything else
   // One side of relay is connected to power, the other to RelayPin
   // Thus, driving RelayPin low turns on the relay
-  if( strcmp( lineBuf, "DustFree" ) == 0 )
+  if( strcmp( Buf, "HB\n" ) == 0 )
+  {
+    // Toggle between small and large hearts to show beating
+    if( HBstat[ index ] == false )
+      HBstat[ index ] = true;
+    else
+      HBstat[ index ] = false;
+    drawHB( index);
+    Serial.print(" HB");
+    Serial.print( index );
+    Serial.print( " " );
+  }
+  else if( strcmp( Buf, "DustFree\n" ) == 0 )
   {
     // Turn on the relay
-    Serial.println("Relay On!");
-    digitalWrite (RelayPin, LOW);
+    //Serial.println( "On!" );
+    digitalWrite ( RelayPin, LOW );
+    // Set and Display the Status
+    On[ index ] = true;
+    drawStatus( index );
   }
-
-  if( strcmp( lineBuf, "Dusty" ) == 0 )
+  else if( strcmp( Buf, "Dusty\n" ) == 0 )
   {
-    // Turn on the relay
-    Serial.println("Relay Off!");
-    digitalWrite (RelayPin, HIGH);
+    // Set and Display the Status
+    On[ index ] = false;
+    drawStatus( index );
+
+    TurnOff();  // Turn off if no client is asking for On
   }
-
-  Telnet.stop();
-
-  lineIdx = 0;
+  Clients[ index ].print( "Ack\n" );  // Try to keep the connection up
 }
-
-int loopCount = 0;
+//-------------------------------------------------------------------
+void AddClient( int index )
+{
+  // Accept a newclient  
+  Clients[ index ] = TelnetServer.accept();
+  //Clients[ i ] = TelnetServer.available();
+  Serial.print("New client: ");
+  Serial.println( index );  
+}
+//-------------------------------------------------------------------
+unsigned long loopCount = 0;
 
 void loop()
 {
-  // See if we got any wireless data
-  handleTelnet();
-
-  delay(200); // Give recipient time to process any transmissions
-
-  // Restart WiFi and the display periodically
-  loopCount++;
-  if( loopCount >= 500 ) // > 10 seconds with 200mS delay above
+  // See if there are any new clients
+  if( TelnetServer.hasClient() )
   {
-    Serial.println( "Restarting Display" );
-    StartDisplay();
-    Serial.println( "Restarting WiFi" );
-    //ESP.restart();    // Can't reboot - need to rpeserve the relay state
-    Telnet.stop();
-    WiFi.disconnect();
-    startAP();   // startup is async
-    delay( 1000 );
-    TelnetServer.begin( );
-    delay( 1000 );
-    loopCount = 0; 
+    // find an empty slot if there is one
+    int i;
+    for( i = 0; i < MAX_CLIENTS; i++ )
+    {
+      if (!Clients[ i ] || !Clients[i].connected() )
+      {
+        if( Clients[ i ] )
+          Clients[ i ].stop();
+        On[ i ] = false;
+        Clients[ i ] = TelnetServer.accept();
+        if( !Clients[ i ] )
+          Serial.println("available broken");
+        Serial.print( "New client: " );
+        Serial.print( i ); Serial.print(' ');
+        Serial.println( Clients[ i ].remoteIP() );
+        break;
+      }      
+    }
+    // Maximum number of clients already connected
+    if( i == MAX_CLIENTS )
+    {
+      //TelnetServer.accept().println("busy");
+      TelnetServer.accept().stop();
+      // hints: server.accept() is a WiFiClient with short-term scope
+      // when out of scope, a WiFiClient will
+      // - flush() - all data will be sent
+      // - stop() - automatically too
+      Serial.println( "Max # of clients already connected" );
+    }
   }
 
-  delay( 1000 );
+  // Check for incoming data from each connected client
+  for( int i = 0; i < MAX_CLIENTS; i++ )
+  {
+    if( Clients[ i ] && Clients[ i ].connected() )
+    {
+      if( Clients[ i ].available() )
+      {
+        while( Clients[ i ].available() )
+        {
+          yield();
+          
+          // Get input one character at a time
+          char c =  Clients[ i ].read();
+          //Serial.write( c );
+
+          // Save the input character and increment the buffer index
+          bufs[ i ].buf[ bufs[ i ].index ] = c;
+          bufs[ i ].index++;
+          bufs[ i ].buf[ bufs[ i ].index ] = '\0'; // Null terminate
+
+          // Check for newline or carriage return
+          if( ( c == '\n' ) || ( c == '\r' ) )
+          {
+            processLine( bufs[ i ].buf, i );
+            bufs[ i ].index = 0;
+          }
+
+          // Check for overflow
+          if( bufs[ i ].index >= MAX_LINE )
+          {
+            Serial.print( "Buffer Overflow, Index = " );
+            Serial.print( i );
+          }
+      
+          // Update the heartbeat timers since we got data from this client
+          ClientHB[ i ] = millis();
+        }
+      }
+    }
+    else
+    {
+      if( Clients[ i ] )
+        Clients[ i ].stop();
+      HBstat[ i ] = false;
+      On[ i ] = false;
+    }
+  }
+
+  // Have we lost heartbeat from any of the clients?
+  for( int i = 0; i < MAX_CLIENTS; i++ )
+  {
+    if( !Clients[ i ] || !( Clients[ i ].connected() ) )
+    {
+      continue;
+    }
+    if( ( millis() - ClientHB[ i ] ) > HB_INTERVAL  )
+    { 
+      Serial.print( "Lost HB: " );
+      Serial.print( i );
+      Clients[ i ].stop();
+      HBstat[ i ] = false;
+      On[ i ] = false;
+      TurnOff();    // Turn off if no client is requesting On
+      drawHB( i );
+      drawStatus( i );
+    }
+    yield();
+  }
+
+  // Print the number of connections for debugging
+  if( loopCount > 10000 )
+  {
+    loopCount = 0;
+    int Connected = 0;
+    for( int i = 0; i < MAX_CLIENTS; i++ )
+    {
+      if( Clients[ i ] && ( Clients[ i ].connected() ) )
+        Connected++;
+    }
+    display.setColor( BLACK );
+    display.fillRect( 0, 16, 128, 16 );
+    display.setColor( WHITE );
+    display.drawString( 0, 16, String( Connected ) );
+    display.drawString( 10, 16, "Connected" );
+    display.display();
+
+#ifdef SERIAL_DEBUG   
+    Serial.print( "Connections: " );
+    Serial.println( Connected );
+#endif
+  }
+  
+  loopCount++;
 }
--- DustCollectorServo.ino
+++ DustCollectorServo.ino
@@ -20,6 +20,18 @@
 #include <ESP8266WiFi.h>
 #include <Servo.h>
 
+#define ROTATION 270
+
+#if ROTATION == 270
+// 270 degree servo values
+#define MIN_ROT 1000
+#define MAX_ROT 1700    // 1667 is calculated value, but go a little extra for mechanical slop
+#else
+// Assume 180 degree servo if not 270
+#define MIN_ROT 1000
+#define MAX_ROT 2000    // 
+#endif
+
 #define AP_SSID "DustCollector"
 #define AP_PWD "Breathe"
 
@@ -32,24 +44,32 @@ Servo GateServo;
 // Analog input pin for current measurements
 #define ANALOG_IN A0
 
-// Digital Pins that set the ID (0 to 15) for this node
+// Digital Pins that set the ID (0 to 3) for this node
+// The Node ID is not used in the current version
 #define ID0 12
-#define ID1 13
-#define ID2 14
-//#define ID3 14
+#define ID1 14
+#define MANUAL 13   // Manual activation pin
 
-int NodeID; // ID of this node (0-15), set by jumpers
+int NodeID; // ID of this node (0-3), set by jumpers. For future use.
 
 // Number of samples to include in the average
-#define AVG_COUNT 100
+#define AVG_COUNT 10
 
 // Zero current Analog In value
 #define ZERO_CURRENT 512
+
+// Current Threshold - turn on above this value
+#define CUR_THRESH 40
 
 // Set these to match the Dust Collector Relay
 char APssid[] = AP_SSID;
 char APpassword[] = AP_PWD;
 int port = PORT;
+
+// Heartbeat variables
+unsigned long LastHB;     // Time in mS since last heartbeat
+#define HB_INTERVAL 300  // Interval in mS between heartbeats
+char HB[] = "HB\n";
 
 // Flag to indicate we were just on
 bool CollectorOn;
@@ -66,6 +86,7 @@ void ConnectTelnet()
   if( Telnet.connect( Server, 23 ) )
   {
     Serial.println( "Telnet Connected" );
+    Telnet.setNoDelay( true );
   }
   else
   {
@@ -75,13 +96,18 @@ void ConnectTelnet()
 
 void ConnectWiFi()
 {
-  //bool Auto = WiFi.getAutoConnect();
-  //Serial.print( "Autoconnect: " );
-  //Serial.println( Auto );
-  
-  WiFi.setAutoConnect( false );     // Reconnect via code  
-  WiFi.hostname( "APssid" );        // This is necessary, but not clear why  
+  WiFi.setAutoConnect( true );
+  delay( 100 );
+  //WiFi.setAutoConnect( false );     // Reconnect via code
+  WiFi.setSleepMode( WIFI_NONE_SLEEP ); 
+  delay( 100 );
+  WiFi.hostname( "APssid" );        // This is necessary, but not clear why 
+  delay( 100 );
+  WiFi.disconnect();
+  delay( 100 );
   WiFi.begin( APssid );             // Connect to the network
+
+  delay( 1000 );  // Forum suggestion to avoid connection drops
 
   Serial.print("Connecting to ");
   Serial.print( APssid ); Serial.println(" ...");
@@ -98,12 +124,11 @@ void ConnectWiFi()
      return;
     
   Serial.println('\n');
-  Serial.println("Connection established!");  
-  Serial.print("IP address:\t");
+  Serial.println("Connection, ");  
+  Serial.print("IP:\t");
   Serial.println( WiFi.localIP() );         // Send the IP address of the ESP8266 to the computer
 
   delay( 100 );
-  ConnectTelnet(); 
 }
 
 // Read the current from the ACS712 sensor.
@@ -128,42 +153,51 @@ int GetRMScurrent()
 
 void setup()
 {
-  Serial.begin(9600);         // Start the Serial communication to send messages to the computer
+  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
   delay(100);
   Serial.println("Setting Up!\n");
 
-  // Set up the Servo
-  //GateServo.attach( SERVO_PIN );
-  
   // Set Up the Node ID pins and calculate the Node ID
   pinMode( ID0, INPUT_PULLUP );
   pinMode( ID1, INPUT_PULLUP );
-  pinMode( ID2, INPUT_PULLUP );
-  //pinMode( ID3, INPUT_PULLUP );
-  Serial.print( "ID0:" ); Serial.println(digitalRead( ID0 ));
-  Serial.print( "ID1:" ); Serial.println(digitalRead( ID1 ));
-  Serial.print( "ID2:" ); Serial.println(digitalRead( ID2 ));
-  //Serial.print( "ID3:" ); Serial.println(digitalRead( ID3 ));
+  pinMode( MANUAL, INPUT_PULLUP );
+
+  Serial.print( "ID0:" ); Serial.println( digitalRead( ID0 ) );
+  Serial.print( "ID1:" ); Serial.println( digitalRead( ID1 ) );
+  Serial.print( "Manual:" ); Serial.println( digitalRead( MANUAL ) );
+
   NodeID = 0;
   NodeID = NodeID + digitalRead( ID0 );
   NodeID = NodeID + ( (digitalRead( ID1 ) << 1 ) & 0x02 );
-  NodeID = NodeID + ( (digitalRead( ID2 ) << 2 ) & 0x04 );
-  //NodeID = NodeID + ( (digitalRead( ID3 ) << 3 ) & 0x08 );
-  NodeID = NodeID ^ 0x07; // Invert. No connects pulled high. Need jumper to pulldown.
+  NodeID = NodeID & 0x03; // Invert. No connects pulled high. Need jumper to pulldown.
 
   Serial.print( "Node ID: " );
   Serial.println( NodeID );
 
   GateServo.attach( SERVO_PIN );
-  GateServo.writeMicroseconds( 1000 );    // Starting position is 45* - stay away from ends of servo rotation
-  
-  ConnectWiFi();
+  GateServo.writeMicroseconds( MIN_ROT );    // Starting position ball valve closed
+
+  // Rotate the valve a bit so we know the servo is working
+  delay( 200 );
+  GateServo.writeMicroseconds( MIN_ROT + 300 );    // Open a bit
+  delay( 200 );
+  GateServo.writeMicroseconds( MIN_ROT );    // Starting position ball valve closed
 
   CollectorOn = false;
+
+  Telnet.setNoDelay( true );
+  ConnectWiFi();
+
+  LastHB = millis();
+
 }
+
+long loopCount = 0;
 
 void loop()
 {
+  unsigned long HoldTime;
+ 
   // In Case of a WiFi disconnection, reconnect
   if( WiFi.status() != WL_CONNECTED )
   {
@@ -171,37 +205,72 @@ void loop()
   }
   else
   {
-    if( Telnet.connected() != true )
+    if( !Telnet.connected() )
     {
       ConnectTelnet();
     }
   }
 
-  // Is the machine turned on? I.e., is it pulling any current?
-  if( GetRMScurrent() > 80 )
+  // Are there characters coming from the server?
+  while( Telnet.available() )
   {
-    Serial.println( "Machine is On!" );
-    GateServo.writeMicroseconds( 2000 ); // Open the blast gate
+    char c = Telnet.read(); // Ignore them. They are there to maintain the connection
+  }
+
+  // Do we need to send a heartbeat?
+  if( ( millis() - LastHB ) > HB_INTERVAL )
+  {
+    Telnet.print( HB );
+    Telnet.flush();
+    LastHB = millis();        
+  }
+
+  int RMS = GetRMScurrent();
+  Serial.println();
+  Serial.print( "RMS: ");
+  Serial.println( RMS );
+
+  // Is the machine turned on? I.e., is it pulling any current?
+  // Or has the manual override been activated?
+  if( ( GetRMScurrent() > CUR_THRESH ) || ( digitalRead( MANUAL ) == 0 ) )
+  {
+    GateServo.writeMicroseconds( MAX_ROT ); // Open the blast gate
     Telnet.print( "DustFree\n");
+    Telnet.flush();
+    Serial.println("Turning On!" );
     CollectorOn = true;
   }
   else
   {
-    Serial.println( "Machine is Off." );
     if( CollectorOn == true )
     {
-      delay( 5000 );  // Keep vacuum on a while to clear hoses
-      GateServo.writeMicroseconds( 1000 );  // Close the blast gate
+        // Keep vacuum on a while to clear hoses
+      HoldTime = millis();
+      while( ( millis() - HoldTime ) < 5000 )
+      {
+        if( ( millis() - LastHB ) > HB_INTERVAL )
+        {
+          Telnet.print( HB );
+          Telnet.flush();
+          LastHB = millis();        
+        }
+      }
+      //delay( 5000 );
+      GateServo.writeMicroseconds( MIN_ROT );  // Close the blast gate
       Telnet.print( "Dusty\n");
+      Telnet.flush();
     }
     Telnet.print( "Dusty\n");
+    Telnet.flush();
     CollectorOn = false;
   }
-  
+#ifdef SHOW_MANUAL  
   // Wait a small amount of time. Note that GetRMScurrent takes > 100mS
-  delay( 10 );
-  Serial.print(".");
-
-  
-
+  //delay( 10 );
+  if( ( loopCount++ % 100 ) == 0 )
+  {
+    Serial.print(".");
+    Serial.print( "Manual:" ); Serial.println( digitalRead( MANUAL ) );
+  }
+#endif 
 }
